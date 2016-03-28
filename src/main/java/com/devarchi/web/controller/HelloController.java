5035c28df937ef0884abaf98e919ee9463f0938f
skyfly33 <skyfly33@github.com>
2016-03-28 17:43:55 +0900

    1. daterange input 필드에 들어온 값을 controller 로 전달후 result 리턴 받기 완료.

--- src/main/java/com/devarchi/web/controller/HelloController.java
+++ src/main/java/com/devarchi/web/controller/HelloController.java
@@ -2,10 +2,14 @@ package com.devarchi.web.controller;
 
 import com.devarchi.web.dao.MemberRepository;
 import com.devarchi.web.dto.Member;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -16,6 +20,8 @@ import java.util.List;
  */
 @Controller
 public class HelloController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     private final String APP_DIR = "home/";
 
@@ -34,10 +40,17 @@ public class HelloController {
         return APP_DIR + "index";
     }
 
-    @RequestMapping("manmonth")
+    @RequestMapping("manMonth")
     public String manMonth(Model model) {
         model.addAttribute("greeting", "Hello");
         return APP_DIR + "manmonth";
+    }
+
+    @RequestMapping(value = "calcManMonth", method = RequestMethod.POST)
+    @ResponseBody
+    public String calcManMonth(@RequestParam String duration) {
+        logger.info("Input Duration: " + duration);
+        return "ManMonth result: " + duration;
     }
 
     @RequestMapping("list")
